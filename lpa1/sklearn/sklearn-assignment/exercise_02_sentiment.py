eb3d956b8c33449686a5f516349dfc199c9b0719
Brunno Oliveira <brunnokick@gmail.com>
2020-06-28 22:13:52 -0300

    Adicionados modelos B e C e restruturação de código

--- lpa1/sklearn/sklearn-assignment/exercise_02_sentiment.py
+++ lpa1/sklearn/sklearn-assignment/exercise_02_sentiment.py
@@ -9,14 +9,23 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 # %%
 random.seed(42)
 np.random.seed(42)
 
+
+# %% [markdown]
+## Common Funcions
+
 # %%
-def get_train_test_data(verbose=True):
-    data_path = r"lpa1/sklearn/sklearn-assignment/data"
+def get_train_test_data(verbose=True, 
+    data_path=r"lpa1/sklearn/sklearn-assignment/data"):
     movie_reviews_data_folder = data_path
     dataset = load_files(movie_reviews_data_folder, shuffle=False)
     x_train, x_test, y_train, y_test = train_test_split(
@@ -27,19 +36,175 @@ def get_train_test_data(verbose=True):
         print(f"Test data: features: {len(x_test)} | target: {len(y_test)}")
     return x_train, x_test, y_train, y_test
 
-# %%
-x_train, x_test, y_train, y_test = get_train_test_data()
 
 # %%
-pipeline_a = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', MultinomialNB())])
+def grid_search(model, x_train, y_train):
+    gs = GridSearchCV(model(), model.params(), n_jobs=-1)
+    gs = gs.fit(x_train, y_train)
+    print(f'Best score: {gs.best_score_} \n Best Params: {gs.best_params_}')
+    # results = pd.DataFrame(gs.cv_results_()
+    return gs
 
-pipeline_a.fit(x_train, y_train)
-predicted = pipeline_a.predict(x_test)
 
-print(f'Acertos: {np.mean(predicted == y_test) * 100}%')
-print('\n\n')
-print("Classification report for classifier %s:\n%s\n"
-      % (pipeline_a, metrics.classification_report(y_test, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(y_test, predicted))
+# %%
+def print_metrics(model, predicted, y_test):
+    print(f'Acertos: {round(np.mean(predicted == y_test) * 100,2)}%')
+    print()
+    print("Classification report for classifier %s:\n%s\n"
+          % (model, metrics.classification_report(y_test, predicted)))
+    print()
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(y_test, predicted))
+
+# %%
+class ModelBase:
+    def __init__(self):
+        self.model = None
+
+    def __call__(self):
+        return self.model
+
+    def fit(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x_test):
+        return self.model.predict(x_test)
+
+
+# %%
+### Pipeline C
+class Model_C(ModelBase):
+    def __init__(self):
+        super().__init__()
+        self.model = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', DecisionTreeClassifier())])
+
+    def params(self):
+        return {
+            'tfidf__norm': ['l1', 'l2', 'max'],
+            'tfidf__use_idf': (False, True),
+            'tfidf__smooth_idf': (False, True),
+            'tfidf__sublinear_tf': (False, True),
+            'clf__criterion' : ["gini", "entropy"],
+            'clf__splitter' : ["best", "random"]
+        }
+
+
+# %% [markdown]
+## Pipelines
+### Pipeline A
+
+# %%
+class Model_A:
+    def __init__(self):
+        self.model = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultinomialNB())])
+
+    def __call__(self):
+        return self.model
+
+    def fit(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x_test):
+        return self.model.predict(x_test)
+
+    def params(self):
+        return {
+            'tfidf__norm': ['l1', 'l2', 'max'],
+            'tfidf__use_idf': (False, True),
+            'tfidf__smooth_idf': (False, True),
+            'tfidf__sublinear_tf': (False, True),
+            # 'clf__alpha': [v/10 for v in range(11)], não é possível usar alpha < 1.0
+            'clf__fit_prior': (False, True)
+        }
+
+
+# %%
+class Model_B:
+    def __init__(self):
+        self.model = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', LinearSVC())])
+
+    def __call__(self):
+        return self.model
+
+    def fit(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x_test):
+        return self.model.predict(x_test)
+
+    def params(self):
+        return {
+            'tfidf__norm': ['l1', 'l2', 'max'],
+            'tfidf__use_idf': (False, True),
+            'tfidf__smooth_idf': (False, True),
+            'tfidf__sublinear_tf': (False, True),
+            'clf__penalty' : ['l1', 'l2'],
+            'clf__loss' : ['hinge', 'squared_hinge'],
+            'clf__dual': (False, True),
+            #'clf__C' : [.0, .5, .1],
+            #'clf__multi_class ': ['ovr', 'crammer_singer']
+        }
+
+
+# %% [markdown]
+## Runner
+
+# %%
+x_train, x_test, y_train, y_test = get_train_test_data(data_path=r'data/')
+
+# %% [markdown]
+### Modelo A
+
+# %%
+# Default Params
+model_a = Model_A()
+model_a.fit(x_train, y_train)
+predicted = model_a.predict(x_test)
+print_metrics(model_a, predicted, y_test)
+
+# %%
+# Best Features
+gs = grid_search(model_a, x_train, y_train)
+best_params_predicted = gs.predict(x_test)
+print_metrics(gs, best_params_predicted, y_test)
+
+
+# %% [markdown]
+### Modelo B 
+
+# %%
+# Default Params
+model_b = Model_B()
+model_b.fit(x_train, y_train)
+predicted = model_b.predict(x_test)
+print_metrics(model_b, predicted, y_test)
+
+# %%
+# Predict with best features
+gs = grid_search(model_b, x_train, y_train)
+best_params_predicted = gs.predict(x_test)
+print_metrics(gs, best_params_predicted, y_test)
+
+# %% [markdown]
+### Modelo C
+
+# %%
+# Default Params
+model_c = Model_C()
+model_c.fit(x_train, y_train)
+predicted = model_c.predict(x_test)
+print_metrics(model_c, predicted, y_test)
+
+# %%
+# Predict with best features
+gs = grid_search(model_c, x_train, y_train)
+best_params_predicted = gs.predict(x_test)
+print_metrics(gs, best_params_predicted, y_test)
